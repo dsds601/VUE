0488d11498f3c2849234fb79b68777663690f514
dsds601 <dsds601@naver.com>
2023-01-08 20:36:43 +0900

    first commit

---
+++ vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter",
+  methods : {
+    clearTodo : function () {
+      this.$store.commit('clearAll');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: #fff;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
